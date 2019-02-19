835938df708a8942c5770b5fa727bad89174af81
TheFloatingBrain <TheFl@LAPTOP-UMJQFRCR>
2019-02-19 00:28:02 -0500

    Some code cleanup.

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,7 @@
 #include <cheerp/webgl.h>
 #include <vector>
 #include <math.h>
+#include <utility>
 #define PI_M 3.1415f
 #define TAU_M ( 2.0f * PI_M )
 
@@ -88,7 +89,7 @@ void PushPoint( std::vector< float >&& toPush, float x, float y, float z )
 
 std::vector< float > GenerateGear( float scale, float toothLength )
 {
-	std::vector< float > circle;
+	std::vector< float > gear;
 	const float POINTS_C = 20.f;
 	const float DELTA_C = TAU_M / POINTS_C;
 	float lastX = cos( 0 ) * scale;
@@ -96,33 +97,25 @@ std::vector< float > GenerateGear( float scale, float toothLength )
 	for( float i = 0.f; i < POINTS_C; i += 1.f )//POINTS_C; i += 1.f )
 	{
 		//Point on circle.//
-		circle.push_back( 0.f * scale );
-		circle.push_back( 0.f * scale );
-		circle.push_back( 0.f * scale );
-		circle.push_back( lastX );
-		circle.push_back( lastY );
-		circle.push_back( 0.f * scale );
-		circle.push_back( lastX = ( cos( DELTA_C * ( i + 1.f ) ) * scale ) );
-		circle.push_back( lastY = ( sin( DELTA_C * ( i + 1.f ) ) * scale ) );
-		circle.push_back( 0.f * scale );
+		PushPoint( std::move( gear ), 0.f * scale, 0.f * scale, 0.f * scale );
+		PushPoint( std::move( gear ), lastX, lastY, 0.f * scale );
+		PushPoint( std::move( gear ), lastX = ( cos( DELTA_C * ( i + 1.f ) ) * scale ), 
+				lastY = ( sin( DELTA_C * ( i + 1.f ) ) * scale ), 0.f * scale );
 		//Tip on gear.//
-		circle.push_back( lastX );
-		circle.push_back( lastY );
-		circle.push_back( 0.f * scale );
-		circle.push_back( cos( DELTA_C * i ) * scale );
-		circle.push_back( sin( DELTA_C * i ) * scale );
-		circle.push_back( 0.f * scale );
-		circle.push_back( cos( DELTA_C * ( i + .5f ) ) * ( 1.f + toothLength ) * scale );
-		circle.push_back( sin( DELTA_C * ( i + .5f ) ) * ( 1.f + toothLength ) * scale );
-		circle.push_back( 0.f * scale );
+		PushPoint( std::move( gear ), lastX, lastY, 0.f * scale );
+		PushPoint( std::move( gear ), cos( DELTA_C * i ) * scale, 
+				sin( DELTA_C * i ) * scale, 0.f * scale );
+		PushPoint( std::move( gear ), 
+				cos( DELTA_C * ( i + .5f ) ) * ( 1.f + toothLength ) * scale, 
+				sin( DELTA_C * ( i + .5f ) ) * ( 1.f + toothLength ) * scale, 0.f * scale );
 	}
-	return circle;
+	return gear;
 }
 
 void webMain()
 {
     	DOMOutput( "<begin>" );
-		auto verts = GenerateGear( .5f, .3f );
+		auto verts = GenerateGear( .5f, .4f );
 		Renderer::Initialize( "glcanvas", verts.data(), verts.size() / 3, 400, 400 );
 		Renderer::Update();
     	DOMOutput( "<end>" );
