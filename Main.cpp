578da6d83d0c98157a6dd2efdbc68cf4b199a344
TheFloatingBrain <TheFl@LAPTOP-UMJQFRCR>
2019-02-19 23:45:14 -0500

    Added hollow gears.

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,7 @@
 void webMain()
 {
     	DOMOutput( "<begin>" );
-		auto verts = GenerateGear( 30.f, .4f, .4f );
+		auto verts = GenerateHollowGear( 30, .8f, .7f, .3f, .5f );
 		Renderer::Initialize( "glcanvas", verts.data(), verts.size() / 3, 400, 400 );
 		Renderer::Update();
     	DOMOutput( "<end>" );
--- Shapes.h
+++ Shapes.h
@@ -4,5 +4,6 @@
 
 #ifndef SHAPES_HEADER_H
 #define SHAPES_HEADER_H
-    std::vector< float > GenerateGear(  unsigned int sides, float scale, float toothLength );
+    std::vector< float > GenerateGear( unsigned int sides, float scale, float toothLength );
+    std::vector< float > GenerateHollowGear( unsigned int sides, float width, float scale, float outerToothLength, float innerToothLength );
 #endif
